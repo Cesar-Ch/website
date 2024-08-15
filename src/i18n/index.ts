8f97bc980df2d9f9967922305dd36c706c5780e0
Cesar-Ch <chafloque1530@gmail.com>
2024-08-14 20:39:16 -0500

    Update language selector icon

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,13 @@
 import spanish from './es.json'
 import english from './en.json'
 
-const LANGUAGES = {
+const LANG = {
     ENGLISH: 'en',
     SPANISH: 'es'
 }
 
 export const getI18N = ({ currentLocale  }: { currentLocale: string }) => {
-    if (currentLocale === LANGUAGES.ENGLISH) return english
-    if (currentLocale === LANGUAGES.SPANISH) return spanish
+    if (currentLocale === LANG.ENGLISH) return english
+    if (currentLocale === LANG.SPANISH) return spanish
     return spanish
 }
